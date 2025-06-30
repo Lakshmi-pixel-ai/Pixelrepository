2de45084e030f3bf38741f333e31ddf2a302295e
Lakshmi-pixel-ai <lakshmi@experience.com>
2025-06-30 12:53:49 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
         {`
           (function() {
             // Configuration
-            const domainKey = '684a741aca1fc468ea05ecfb';
+            const domainKey = '68623b0464e3f7aae34a6a1d';
             const proApiUrl = 'https://proapi.qa.experience.com';
             
             // Utility functions
